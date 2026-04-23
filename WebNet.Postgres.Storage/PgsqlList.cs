e7455811599df689fb9eb63ba661abecf82e77d0
JoJo Webster <151088710+miserix13@users.noreply.github.com>
2026-04-23 12:43:22 -0500

    Add project files.

---
+++ WebNet.Postgres.Storage/PgsqlList.cs
@@ -0,0 +1,28 @@
+﻿using Take.Elephant.Sql.PostgreSql;
+using Take.Elephant.Sql.Mapping;
+
+namespace WebNet.Postgres.Storage
+{
+    public class PgsqlList<T>(string connectionString, ITable table, IMapper<T> mapper) : Take.Elephant.Sql.SqlList<T>(new PostgreSqlDatabaseDriver(), connectionString, table, mapper), Take.Elephant.IList<T>
+    {
+        public override async Task AddAsync(T value)
+        {
+            await base.AddAsync(value);
+        }
+
+        public override IAsyncEnumerable<T> AsEnumerableAsync(CancellationToken cancellationToken = default)
+        {
+            return base.AsEnumerableAsync(cancellationToken);
+        }
+
+        public override async Task<long> GetLengthAsync(CancellationToken cancellationToken = default)
+        {
+            return await base.GetLengthAsync(cancellationToken);
+        }
+
+        public override async Task<long> RemoveAllAsync(T value)
+        {
+            return await base.RemoveAllAsync(value);
+        }
+    }
+}
